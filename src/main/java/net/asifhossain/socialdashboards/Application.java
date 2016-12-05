d8bcfe5e9b33045b6c4b9266f055ac543a7e3b15
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2016-12-05 16:57:41 +0600

    Initial commit add facebook connect to application

---
+++ src/main/java/net/asifhossain/socialdashboards/Application.java
@@ -0,0 +1,15 @@
+package net.asifhossain.socialdashboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author asif.hossain
+ * @since 12/5/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/net/asifhossain/socialdashboards/web/DashboardController.java
@@ -0,0 +1,11 @@
+package net.asifhossain.socialdashboards.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author asif.hossain
+ * @since 12/5/16.
+ */
+@Controller
+public class DashboardController {
+}
